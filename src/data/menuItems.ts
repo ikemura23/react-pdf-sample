c2104b251a20126e9ae1753f6b65ce0a12f68fca
Kazutaka Ikemura <banbara.work@gmail.com>
2025-09-18 20:38:21 +0900

    feat: メニュー項目に新しい配達員を追加し、座標情報を更新しました。

--- src/data/menuItems.ts
+++ src/data/menuItems.ts
@@ -3,5 +3,8 @@ import type { MenuItem } from '../types'
 export const menuItems: MenuItem[] = [
   { name: 'はこすけ 矢野 智大', coordinates: { x: 1056, y: 693 } },
   { name: '河野 裕美', coordinates: { x: 1001, y: 1701 } },
-  { name: 'WBC.Plus B-1 水上 伊織', coordinates: { x: 268, y: 6664 } }
+  { name: 'WBC.Plus B-1 水上 伊織', coordinates: { x: 268, y: 6664 } },
+  { name: 'WBC.Plus B-2 片岡 慎也', coordinates: { x: 254, y: 6664 } },
+  { name: 'WBC.Plus B-4 内田 恵美', coordinates: { x: 263, y: 6664 } },
+  { name: '藤井孝信', coordinates: { x: 800, y: 6592 } },
 ]
